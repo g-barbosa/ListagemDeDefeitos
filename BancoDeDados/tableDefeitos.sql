6137ebcf7505f2ff9d4203e9697bc636428fea11
g-barbosa <giovanne.bbarbosa@gmail.com>
2019-06-21 21:31:55 -0300

    Banco de dados

---
+++ BancoDeDados/tableDefeitos.sql
@@ -0,0 +1,10 @@
+CREATE TABLE defeitos (
+	equipamento varchar(10),
+	placa varchar(10),
+	componente varchar(10),
+	posicao varchar(5),
+	defeito varchar(255)
+);
+
+insert into defeitos (equipamento,placa,componente,posicao,defeito) values ('a','b','c','d','e');
+
